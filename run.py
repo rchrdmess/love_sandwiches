b90359542ba5736069a31bcb2d4b21959d482de3
Richard Messenger <rchrdmessenger@gmail.com>
2024-02-05 16:11:31 +0000

    Updating stock data function.

--- run.py
+++ run.py
@@ -90,6 +90,21 @@ def get_last_five_sales_entries():
         columns.append(column[-5:])
     return columns
 
+def calculate_stock_data(data):
+    """
+    Calculating the average stock for each sandwich type.
+    """
+    print("Calculating stock data...\n")
+    new_stock_data = []
+
+    for column in data:
+        int_column = [int(num) for num in column]
+        average = sum(int_column) / len(int_column)
+        stock_num = average * 1.1
+        new_stock_data.append(round(stock_num))
+
+    return new_stock_data
+
 def main ():
     """
     Run all program functions.
@@ -99,8 +114,9 @@ def main ():
     update_worksheet(sales_data, 'sales')
     new_surplus_data = calculate_surplus_data(sales_data)
     update_worksheet(new_surplus_data, 'surplus')
+    sales_columns = get_last_five_sales_entries()
+    stock_data = calculate_stock_data(sales_columns)
+    update_worksheet(stock_data, 'stock')
 
 print("Welcome to data automation.\n")
-# main()
-
-sales_columns = get_last_five_sales_entries()
+main()
